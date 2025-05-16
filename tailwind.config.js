8d4faff275299943c2fa2cff7c5970bc3ce6dd5c
Imeh Usoro <imeusoro@rocketmail.com>
2025-05-16 10:11:12 +0100

    pre-init

---
+++ tailwind.config.js
@@ -0,0 +1,97 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+  	extend: {
+  		colors: {
+  			warm: {
+  				'50': '#FFE3DE',
+  				'100': '#FFAD97',
+  				'200': '#FF6F00',
+  				'300': '#C45400',
+  				'400': '#8D3A00',
+  				'500': '#592200',
+  				'600': '#2A0C00'
+  			},
+  			peach: {
+  				'50': '#FFECE8',
+  				'100': '#FFB6A2',
+  				'200': '#FF7A35',
+  				'300': '#CC5900',
+  				'400': '#943F00',
+  				'500': '#602600',
+  				'600': '#301000'
+  			},
+  			coral: {
+  				'50': '#FFEDE7',
+  				'100': '#FFC4AD',
+  				'200': '#FF8C33',
+  				'300': '#CD6A00',
+  				'400': '#984D00',
+  				'500': '#653100',
+  				'600': '#371800'
+  			},
+  			neutral: {
+  				'50': '#EBE9E9',
+  				'100': '#C5C1C0',
+  				'200': '#A39997',
+  				'300': '#7C7573',
+  				'400': '#585251',
+  				'500': '#363231',
+  				'600': '#171514'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
